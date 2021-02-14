d1b46dcc352f29323a0eb33e4be3cc6f0c7e18c7
Andres Guerrero <anguerrero@googlemail.com>
2021-02-14 14:42:00 +0100

    antes de irnos a almorzar

--- src/main/java/org/chilin/TtrInvestigatorRunner.java
+++ src/main/java/org/chilin/TtrInvestigatorRunner.java
@@ -1,13 +1,26 @@
 package org.chilin;
 
+import org.chilin.config.MyTTConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TtrInvestigatorRunner {
+public class TtrInvestigatorRunner implements CommandLineRunner {
+
+    @Autowired
+    private MyTTConfig myTTConfig;
 
     public static void main( String[] args ){
         SpringApplication.run(TtrInvestigatorRunner.class);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("using environment: " + myTTConfig.getEnvironment());
+        System.out.println("name: " + myTTConfig.getName());
+        System.out.println("enabled:" + myTTConfig.isEnabled());
+        System.out.println("servers: " + myTTConfig.getServers());
+    }
 }
